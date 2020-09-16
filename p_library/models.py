ce3f128b15dd25519b8b62968b3dbe3a4b2ce09c
Vyacheslav <programmer38rus@gmail.com>
2020-09-16 17:46:26 +0800

    n

--- p_library/models.py
+++ p_library/models.py
@@ -5,15 +5,16 @@ class Author(models.Model):
     full_name = models.TextField()
     birth_year = models.SmallIntegerField()
     country = models.CharField(max_length=2)
-    copy_count =  models.SmallIntegerField()
+    copy_count =  models.SmallIntegerField(default=1)
 
 class Book(models.Model):
     ISBN = models.CharField(max_length=13)
     title = models.TextField()
     description = models.TextField()
     year_release = models.SmallIntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     price = models.FloatField()
+    copy_count =  models.SmallIntegerField(default=1)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 
 
