225f3c74f461272d1a52bd61119ab3ce7c6dabf9
Vyacheslav <programmer38rus@gmail.com>
2020-09-18 10:50:09 +0800

    n

--- p_library/views.py
+++ p_library/views.py
@@ -12,22 +12,33 @@ def books_list(request):
     return HttpResponse(list)
 
 def index(request):
-    price_count = 2284.66
+    book_max_price = 0
+    book_min_price = 0
+    book_min = None 
 
     template = loader.get_template('index.html')
     books_count = Book.objects.all().count()
     books = Book.objects.all()
-    
+
+    new = sum([book.price for book in books if book.copy_count > 1])
+
     for book in books:
-        if book.price < price_count:
-            price_count = book.price
+        if book.price > book_max_price:
+            book_max_price = book.price
+    book_min_price = book_max_price
+    for book in books:
+        if book.price < book_min_price:
+            book_min_price = book.price
+            book_min = book
 
     list_num = [i for i in range(1, 101)]
     biblio_data = {
         "title": "мою библиотеку", 
         "books": books,
         "list_num": list_num,
-        "work": price_count,
+        "book_max_price": book_max_price,
+        "book_min": book_min,
+        "book_summ_if": new,
         }
 
     return HttpResponse(template.render(biblio_data))
