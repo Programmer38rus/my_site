94dc65af11e1d45e6431ef6e00de8c51b4decad7
Vyacheslav <programmer38rus@gmail.com>
2020-09-22 17:23:35 +0800

    m

--- p_library/admin.py
+++ p_library/admin.py
@@ -4,7 +4,14 @@ from p_library.models import Book, Author
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'copy_count')
+
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+
+    list_display = ('title', 'author_full_name', 'copy_count')
+    fields = (('title', 'description'), 'year_release', 'author', 'price')
+    exclude = ('ISBN',)
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
--- p_library/views.py
+++ p_library/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from p_library.models import Author, Book
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect
 
 # Create your views here.
 def books_list(request):
@@ -77,4 +78,41 @@ def index(request):
         "vot5": books_not_one,
         }
 
-    return HttpResponse(template.render(biblio_data))
+    return HttpResponse(template.render(biblio_data, request))
+
+
+def book_increment(request):
+  if request.method == 'POST':
+    book_id = request.POST['id']
+    if not book_id:
+      return redirect('/index/')
+    else:
+      book = Book.objects.filter(id=book_id).first()
+      if not book:
+        return redirect('/index/')
+      
+      book.copy_count +=1
+      book.save()
+    return redirect('/index/')
+  else:
+    return redirect('/index/')
+
+
+
+def book_decrement(request):
+  if request.method == 'POST':
+    book_id = request.POST['id']
+    if not book_id:
+      return redirect('/index/')
+    else:
+      book = Book.objects.filter(id=book_id).first()
+      if not book:
+        return redirect('/index/')
+      if book.copy_count < 1:
+        book.copy_count = 0
+      else:
+        book.copy_count -= 1
+      book.save()
+    return redirect('/index/')
+  else:
+    return redirect('/index/')
