67f1b8cd85a77d69f80806e390c187075bf5abc1
Vyacheslav <programmer38rus@gmail.com>
2020-09-28 12:20:49 +0800

    new

--- p_library/views.py
+++ p_library/views.py
@@ -4,33 +4,38 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
 
+
 # Create your views here.
 def books_list(request):
     list = []
     books = Book.objects.all()
     for i in books:
-       list.append(i.title) 
+        list.append(i.title)
     return HttpResponse(list)
 
-def publishing_house(request):
-  template = loader.get_template('publishinghouse.html')
-  
-  pub_houses = PublishingHouse.objects.all()
 
-  data = {
-    "pub_houses": pub_houses
-  }
-  return HttpResponse(template.render(data, request))
+def publishing_house(request):
+    template = loader.get_template('publishinghouse.html')
+
+    pub_houses = PublishingHouse.objects.all()
+
+    data = {
+        "pub_houses": pub_houses
+    }
+    return HttpResponse(template.render(data, request))
+
 
 def index(request):
     book_max_price = 0
     book_min_price = 0
-    book_min = None 
+    book_min = None
     dict_author = {}
 
     template = loader.get_template('index.html')
     books_count = Book.objects.all().count()
     books = Book.objects.all()
+
+    pub_houses = PublishingHouse.objects.all()
 
     books_no_ru = 0
     books_pushkin = 0
@@ -42,13 +47,13 @@ def index(request):
         if book.author.country != "RU":
             books_no_ru += book.price * book.copy_count
         if book.author.full_name == "Пушкин Александр Сергеевич":
-          books_pushkin += book.price * book.copy_count
+            books_pushkin += book.price * book.copy_count
         if book.author.full_name == "Douglas Adams":
-          books_duglas += book.price
+            books_duglas += book.price
         if book.copy_count != 1:
-          books_not_one += book.price * book.copy_count
+            books_not_one += book.price * book.copy_count
         if book.copy_count > 1:
-          new += book.price
+            new += book.price
 
     # books_no_ru = sum([book.price*book.copy_count for book in books if book.author.country != "RU"])
     # books_pushkin = sum([book.price*book.copy_count for book in books if book.author.full_name == "Пушкин Александр Сергеевич"])
@@ -57,13 +62,12 @@ def index(request):
 
     # new = sum([book.price for book in books if book.copy_count > 1])
 
-
     # вычисляем максимальну и минимальную стоимость книги.
     for book in books:
 
         if book.price > book_max_price:
             book_max_price = book.price
-        dict_author[book.author.full_name] = book.price * book.copy_count    
+        dict_author[book.author.full_name] = book.price * book.copy_count
 
     max_all_price = sum([v for k, v in dict_author.items()])
     book_min_price = book_max_price
@@ -74,9 +78,9 @@ def index(request):
             book_min = book
 
     list_num = [i for i in range(1, 101)]
-    
+
     biblio_data = {
-        "title": "мою библиотеку", 
+        "title": "мою библиотеку",
         "books": books,
         "list_num": list_num,
         "book_max_price": book_max_price,
@@ -84,47 +88,47 @@ def index(request):
         "book_summ_if": new,
         "vot": dict_author,
         "vot2": books_no_ru,
-        "vot3": books_pushkin, 
+        "vot3": books_pushkin,
         "vot4": books_duglas,
         "vot5": books_not_one,
-        "force": [1,2,3,4,5],
-        }
+        "force": [1, 2, 3, 4, 5],
+        "pub_houses": pub_houses,
+    }
 
     return HttpResponse(template.render(biblio_data, request))
 
 
 def book_increment(request):
-  if request.method == 'POST':
-    book_id = request.POST['id']
-    if not book_id:
-      return redirect('/index/')
-    else:
-      book = Book.objects.filter(id=book_id).first()
-      if not book:
-        return redirect('/index/')
-      
-      book.copy_count +=1
-      book.save()
-    return redirect('/index/')
-  else:
-    return redirect('/index/')
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
 
+            book.copy_count += 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
 
 
 def book_decrement(request):
-  if request.method == 'POST':
-    book_id = request.POST['id']
-    if not book_id:
-      return redirect('/index/')
-    else:
-      book = Book.objects.filter(id=book_id).first()
-      if not book:
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
         return redirect('/index/')
-      if book.copy_count < 1:
-        book.copy_count = 0
-      else:
-        book.copy_count -= 1
-      book.save()
-    return redirect('/index/')
-  else:
-    return redirect('/index/')
+    else:
+        return redirect('/index/')
