c080826cebd422afedf63c59ea9d372d7c7f5334
Vyacheslav <programmer38rus@gmail.com>
2020-10-05 09:20:14 +0800

    new

--- p_library/models.py
+++ p_library/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 class Author(models.Model):
     full_name = models.TextField()
     birth_year = models.SmallIntegerField()
     country = models.CharField(max_length=2)
-    
+
     def __str__(self):
         return self.full_name
-    
+
+
 class PublishingHouse(models.Model):
     full_name = models.CharField(default="Empty", max_length=100)
     count_book = models.IntegerField(null=True)
-    
+
     def __str__(self):
         return self.full_name
+
 
 class Book(models.Model):
     ISBN = models.CharField(max_length=13)
@@ -22,9 +25,10 @@ class Book(models.Model):
     description = models.TextField()
     year_release = models.SmallIntegerField()
     price = models.FloatField()
-    copy_count =  models.SmallIntegerField(default=1)
+    copy_count = models.SmallIntegerField(default=1)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name="Вербос_нэйм")
-    publishing_house = models.ForeignKey(PublishingHouse, on_delete=models.CASCADE, verbose_name="Издательство", null=True, related_name="books", blank=True)
+    publishing_house = models.ForeignKey(PublishingHouse, on_delete=models.CASCADE, verbose_name="Издательство",
+                                         null=True, related_name="books", blank=True)
 
     def __str__(self):
         return self.title
